80f7f1542ac2fe70e0748d073038df1112d3b5d7
yu-skp0518 <reality_0518@yahoo.co.jp>
2021-11-20 15:14:32 +0000

    convert

--- hello.rb
+++ hello.rb
@@ -113,7 +113,7 @@
 #taguchi 200
 #fkoji 400
 
-scores = {"taguchi" => 200, "fkoji" => 400 }
+# scores = {"taguchi" => 200, "fkoji" => 400 }
 # scores = {:taguchi => 200, :fkoji => 400 }
 # scores = {taguchi: 200, fkoji: 400 }
 
@@ -132,3 +132,15 @@ scores = {"taguchi" => 200, "fkoji" => 400 }
 
 # --------------------------------------------------
 
+#変換
+
+x = 50
+y = "3"
+
+p x + y.to_i
+p x + y.to_f
+p x.to_s + y
+
+scores = {taguchi: 200, fkoji: 400}
+
+p scores.to_a.to_h
